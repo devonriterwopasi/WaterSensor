573552b4baf6c9b4ba6f2f42d1e8ec6b31bcb034
devonriterwopasi <88048490+devonriterwopasi@users.noreply.github.com>
2021-08-04 12:09:54 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,5 +1,5 @@
-// link to instructiobns for main code: https://roboticadiy.com/motion-detection-video-captured-email-alert-using-raspberry-pi-4/
-// Code to set pi to run program at start up https://www.dexterindustries.com/howto/auto-run-python-programs-on-the-raspberry-pi/
+# link to instructiobns for main code: https://roboticadiy.com/motion-detection-video-captured-email-alert-using-raspberry-pi-4/
+# Code to set pi to run program at start up https://www.dexterindustries.com/howto/auto-run-python-programs-on-the-raspberry-pi/
         
 from gpiozero import MotionSensor, Button, LED
 from picamera import PiCamera
